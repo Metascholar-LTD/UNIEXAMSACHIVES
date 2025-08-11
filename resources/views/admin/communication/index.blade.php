722ed0a14b5e763ebeddc526d56ccc83b21d70c0
Kyiewu Bernard <kyiewubernard18@gmail.com>
2025-08-11 15:45:31 +0000

    chnage

--- resources/views/admin/communication/index.blade.php
+++ resources/views/admin/communication/index.blade.php
@@ -194,7 +194,19 @@
                                                        class="btn btn-sm btn-info" title="View Details">
                                                         <i class="icofont-eye"></i>
                                                     </a>
-                                                    
+
+                                                    @if($campaign->status !== 'sending')
+                                                        <form method="POST" action="{{ route('admin.communication.destroy', $campaign) }}" 
+                                                              style="display: inline-block;" 
+                                                              onsubmit="return confirm('Are you sure you want to delete this email?')">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit" class="btn btn-sm btn-danger" title="Delete">
+                                                                <i class="icofont-trash"></i>
+                                                            </button>
+                                                        </form>
+                                                    @endif
+
                                                     @if($campaign->status === 'draft')
                                                         <a href="{{ route('admin.communication.edit', $campaign) }}" 
                                                            class="btn btn-sm btn-warning" title="Edit">
@@ -205,22 +217,10 @@
                                                     @if(in_array($campaign->status, ['draft', 'scheduled']))
                                                         <form method="POST" action="{{ route('admin.communication.send', $campaign) }}" 
                                                               style="display: inline-block;" 
-                                                              onsubmit="return confirm('Are you sure you want to send this campaign?')">
+                                                              onsubmit="return confirm('Are you sure you want to send this email?')">
                                                             @csrf
                                                             <button type="submit" class="btn btn-sm btn-success" title="Send Now">
                                                                 <i class="icofont-send-mail"></i>
-                                                            </button>
-                                                        </form>
-                                                    @endif
-
-                                                    @if($campaign->status !== 'sending')
-                                                        <form method="POST" action="{{ route('admin.communication.destroy', $campaign) }}" 
-                                                              style="display: inline-block;" 
-                                                              onsubmit="return confirm('Are you sure you want to delete this campaign?')">
-                                                            @csrf
-                                                            @method('DELETE')
-                                                            <button type="submit" class="btn btn-sm btn-danger" title="Delete">
-                                                                <i class="icofont-trash"></i>
                                                             </button>
                                                         </form>
                                                     @endif
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -256,7 +256,7 @@
                                     <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                                     <polyline points="22,6 12,13 2,6"></polyline>
                                 </svg>
-                                Email Campaigns</a>
+                                Emails</a>
                         </li>
                         <li>
                             <a href="{{route('admin.communication.create')}}">
