95f5cfeb777867709744888f786c6c3149575923
Kyiewu Bernard <kyiewubernard18@gmail.com>
2025-08-22 18:20:23 +0000

    footer redesigned

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,142 +1,136 @@
-<div class="footerarea">
+<footer class="footer-area">
     <div class="container">
-        {{-- <div class="footerarea__wrapper footerarea__wrapper__2">
-            <div class="row">
-                <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12" data-aos="fade-up">
-                    <div class="footerarea__inner footerarea__about__us">
-                        <div class="footerarea__heading">
-                            <h3>About us</h3>
+        <!-- Main Footer Content -->
+        <div class="footer-main">
+            <div class="row g-4">
+                <!-- Company Info -->
+                <div class="col-lg-4 col-md-6">
+                    <div class="footer-widget">
+                        <div class="footer-logo mb-3">
+                            <img src="{{ asset('img/cug_logo_new.jpeg') }}" alt="CUG Logo" class="footer-logo-img" style="max-height: 50px;">
                         </div>
-                        <div class="footerarea__content">
-                            <p>orporate clients and leisure travelers has been relying on Groundlink for dependable safe, and professional chauffeured car end service in major cities across World.</p>
+                        <p class="footer-description">
+                            Empowering academic excellence through innovative digital solutions and comprehensive exam management systems.
+                        </p>
+                        <div class="footer-social">
+                            <a href="#" class="social-link" aria-label="Facebook">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                            <a href="#" class="social-link" aria-label="Twitter">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                            <a href="#" class="social-link" aria-label="LinkedIn">
+                                <i class="fab fa-linkedin-in"></i>
+                            </a>
+                            <a href="#" class="social-link" aria-label="Instagram">
+                                <i class="fab fa-instagram"></i>
+                            </a>
                         </div>
-                        <div class="foter__bottom__text">
-                            <div class="footer__bottom__icon">
-                                <i class="icofont-clock-time"></i>
+                    </div>
+                </div>
+
+                <!-- Quick Links -->
+                <div class="col-lg-2 col-md-6">
+                    <div class="footer-widget">
+                        <h5 class="footer-title">Quick Links</h5>
+                        <ul class="footer-links">
+                            <li><a href="{{ route('frontend.home') }}">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">FAQs</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Services -->
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-widget">
+                        <h5 class="footer-title">Our Services</h5>
+                        <ul class="footer-links">
+                            <li><a href="#">Exam Management</a></li>
+                            <li><a href="#">Digital Archives</a></li>
+                            <li><a href="#">Academic Records</a></li>
+                            <li><a href="#">Student Portal</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Contact Info -->
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-widget">
+                        <h5 class="footer-title">Contact Info</h5>
+                        <div class="contact-info">
+                            <div class="contact-item">
+                                <i class="fas fa-map-marker-alt"></i>
+                                <span>Catholic University of Ghana</span>
                             </div>
-                            <div class="footer__bottom__content">
-                                <h6>Opening Houres</h6>
-                                <span>Mon - Sat(8.00 - 6.00)</span>
-                                <span>Sunday - Closed</span>
+                            <div class="contact-item">
+                                <i class="fas fa-phone"></i>
+                                <span>+233 XX XXX XXXX</span>
+                            </div>
+                            <div class="contact-item">
+                                <i class="fas fa-envelope"></i>
+                                <span>info@cug.edu.gh</span>
                             </div>
                         </div>
-
-                    </div>
-                </div>
-                <div class="col-xl-2 col-lg-2 col-md-6 col-sm-6" data-aos="fade-up">
-                    <div class="footerarea__inner">
-                        <div class="footerarea__heading">
-                            <h3>Usefull Links</h3>
-                        </div>
-                        <div class="footerarea__list">
-                            <ul>
-                                <li>
-                                    <a href="#">About Us</a>
-                                </li>
-                                <li>
-                                    <a href="#">Teachers</a>
-                                </li>
-                                <li>
-                                    <a href="#">Partner</a>
-                                </li>
-                                <li>
-                                    <a href="#">Room-Details</a>
-                                </li>
-                                <li>
-                                    <a href="#">Gallery</a>
-                                </li>
-                            </ul>
-                        </div>
-
-
-                    </div>
-                </div>
-                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6" data-aos="fade-up">
-                    <div class="footerarea__inner footerarea__padding__left">
-                        <div class="footerarea__heading">
-                            <h3>Course</h3>
-                        </div>
-                        <div class="footerarea__list">
-                            <ul>
-                                <li>
-                                    <a href="#">Ui Ux Design</a>
-                                </li>
-                                <li>
-                                    <a href="#">Web Development</a>
-                                </li>
-                                <li>
-                                    <a href="#">Business Strategy</a>
-                                </li>
-                                <li>
-                                    <a href="#">Softwere Development</a>
-                                </li>
-                                <li>
-                                    <a href="#">Business English</a>
-                                </li>
-                            </ul>
-                        </div>
-
-
-                    </div>
-                </div>
-
-                <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12" data-aos="fade-up">
-                    <div class="footerarea__right__wraper footerarea__inner">
-                        <div class="footerarea__heading">
-                            <h3>Recent Post</h3>
-                        </div>
-                        <div class="footerarea__right__list">
-                            <ul>
-                                <li>
-                                    <a href="#">
-                                        <div class="footerarea__right__img">
-                                            <img loading="lazy"  src="img/footer/footer__1.png" alt="footerphoto">
-                                        </div>
-                                        <div class="footerarea__right__content">
-                                            <span>02 Apr 2024 </span>
-                                            <h6>Best Your Business</h6>
-                                        </div>
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="#">
-                                        <div class="footerarea__right__img">
-                                            <img loading="lazy"  src="img/footer/footer__2.png" alt="footerphoto">
-                                        </div>
-                                        <div class="footerarea__right__content">
-                                            <span>02 Apr 2024 </span>
-                                            <h6>Keep Your Business</h6>
-                                        </div>
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="#">
-                                        <div class="footerarea__right__img">
-                                            <img loading="lazy"  src="img/footer/footer__3.png" alt="footerphoto">
-                                        </div>
-                                        <div class="footerarea__right__content">
-                                            <span>02 Apr 2024 </span>
-                                            <h6>Nice Your Business</h6>
-                                        </div>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
-        <div class="footerarea__copyright__wrapper footerarea__copyright__wrapper__2">
-            <div class="row">
-                <div class="col-xl-12 col-lg-12">
-                    <div class="text-center footerarea__copyright__content footerarea__copyright__content__2">
-                        <p>Copyright &copy; <span>{{ date('Y') }}</span> by CUG. All Rights Reserved.</p>
                     </div>
                 </div>
             </div>
         </div>
 
+        <!-- Footer Divider -->
+        <div class="footer-divider"></div>
+
+        <!-- Footer Bottom -->
+        <div class="footer-bottom">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <div class="copyright-text">
+                        <p>&copy; {{ date('Y') }} Catholic University of Ghana. All Rights Reserved.</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="footer-bottom-links text-md-end">
+                        <a href="#">Privacy Policy</a>
+                        <a href="#">Terms of Service</a>
+                        <a href="#">Cookie Policy</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Developer Credits -->
+<div class="developer-credits">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <div class="credits-text">
+                    <span>Developed and Powered by</span>
+                    <a href="mailto:support@academicdigital.space" class="company-link">Metascholar Consult LTD</a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="partner-links text-md-end">
+                    <a href="https://scholarindexing.com/" target="_blank" rel="noopener noreferrer" class="partner-link">
+                        <i class="fas fa-graduation-cap"></i>
+                        <span>Scholar Indexing Society</span>
+                    </a>
+                    <a href="http://academicdigital.space/" target="_blank" rel="noopener noreferrer" class="partner-link">
+                        <i class="fas fa-university"></i>
+                        <span>Metascholar Institute</span>
+                    </a>
+                    <a href="https://metascholarturnitinmoodle.com/" target="_blank" rel="noopener noreferrer" class="partner-link">
+                        <i class="fas fa-check-circle"></i>
+                        <span>Metascholar Turnitin</span>
+                    </a>
+                    <a href="https://ijmsirjournal.com/index.php/ojs" target="_blank" rel="noopener noreferrer" class="partner-link">
+                        <i class="fas fa-book-open"></i>
+                        <span>IJMSIR</span>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
