cddcd6d424943a2f1a2f2f601cd7df8e3b2be619
Kyiewu Bernard <kyiewubernard18@gmail.com>
2025-08-22 11:52:08 +0000

    new footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,124 +1,122 @@
 <div class="footerarea">
     <div class="container">
-        {{-- <div class="footerarea__wrapper footerarea__wrapper__2">
+        <!-- Main Footer Content -->
+        <div class="footerarea__wrapper footerarea__wrapper__2">
             <div class="row">
+                <!-- About Section -->
                 <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12" data-aos="fade-up">
                     <div class="footerarea__inner footerarea__about__us">
                         <div class="footerarea__heading">
-                            <h3>About us</h3>
+                            <h3>About CUG</h3>
                         </div>
                         <div class="footerarea__content">
-                            <p>orporate clients and leisure travelers has been relying on Groundlink for dependable safe, and professional chauffeured car end service in major cities across World.</p>
+                            <p>Copperbelt University is a premier institution of higher learning committed to academic excellence, innovation, and community development. We provide quality education and research opportunities.</p>
                         </div>
-                        <div class="foter__bottom__text">
-                            <div class="footer__bottom__icon">
-                                <i class="icofont-clock-time"></i>
+                        <div class="footerarea__contact">
+                            <div class="contact__item">
+                                <div class="contact__icon">
+                                    <i class="icofont-location-pin"></i>
+                                </div>
+                                <div class="contact__content">
+                                    <span>Jambo Drive, Riverside, Kitwe, Zambia</span>
+                                </div>
                             </div>
-                            <div class="footer__bottom__content">
-                                <h6>Opening Houres</h6>
-                                <span>Mon - Sat(8.00 - 6.00)</span>
-                                <span>Sunday - Closed</span>
+                            <div class="contact__item">
+                                <div class="contact__icon">
+                                    <i class="icofont-phone"></i>
+                                </div>
+                                <div class="contact__content">
+                                    <span>+260 212 290 816</span>
+                                </div>
+                            </div>
+                            <div class="contact__item">
+                                <div class="contact__icon">
+                                    <i class="icofont-envelope"></i>
+                                </div>
+                                <div class="contact__content">
+                                    <span>info@cbu.ac.zm</span>
+                                </div>
                             </div>
                         </div>
-
                     </div>
                 </div>
+
+                <!-- Quick Links Section -->
                 <div class="col-xl-2 col-lg-2 col-md-6 col-sm-6" data-aos="fade-up">
                     <div class="footerarea__inner">
                         <div class="footerarea__heading">
-                            <h3>Usefull Links</h3>
+                            <h3>Quick Links</h3>
                         </div>
                         <div class="footerarea__list">
                             <ul>
-                                <li>
-                                    <a href="#">About Us</a>
-                                </li>
-                                <li>
-                                    <a href="#">Teachers</a>
-                                </li>
-                                <li>
-                                    <a href="#">Partner</a>
-                                </li>
-                                <li>
-                                    <a href="#">Room-Details</a>
-                                </li>
-                                <li>
-                                    <a href="#">Gallery</a>
-                                </li>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Academic Programs</a></li>
+                                <li><a href="#">Research</a></li>
+                                <li><a href="#">Admissions</a></li>
+                                <li><a href="#">Student Life</a></li>
+                                <li><a href="#">Contact</a></li>
                             </ul>
                         </div>
-
-
                     </div>
                 </div>
+
+                <!-- Academic Programs Section -->
                 <div class="col-xl-3 col-lg-3 col-md-6 col-sm-6" data-aos="fade-up">
                     <div class="footerarea__inner footerarea__padding__left">
                         <div class="footerarea__heading">
-                            <h3>Course</h3>
+                            <h3>Academic Programs</h3>
                         </div>
                         <div class="footerarea__list">
                             <ul>
-                                <li>
-                                    <a href="#">Ui Ux Design</a>
-                                </li>
-                                <li>
-                                    <a href="#">Web Development</a>
-                                </li>
-                                <li>
-                                    <a href="#">Business Strategy</a>
-                                </li>
-                                <li>
-                                    <a href="#">Softwere Development</a>
-                                </li>
-                                <li>
-                                    <a href="#">Business English</a>
-                                </li>
+                                <li><a href="#">Engineering</a></li>
+                                <li><a href="#">Business Studies</a></li>
+                                <li><a href="#">Natural Sciences</a></li>
+                                <li><a href="#">Humanities</a></li>
+                                <li><a href="#">Medicine</a></li>
+                                <li><a href="#">Agriculture</a></li>
                             </ul>
                         </div>
-
-
                     </div>
                 </div>
 
+                <!-- News & Updates Section -->
                 <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12" data-aos="fade-up">
                     <div class="footerarea__right__wraper footerarea__inner">
                         <div class="footerarea__heading">
-                            <h3>Recent Post</h3>
+                            <h3>Latest Updates</h3>
                         </div>
                         <div class="footerarea__right__list">
                             <ul>
                                 <li>
                                     <a href="#">
                                         <div class="footerarea__right__img">
-                                            <img loading="lazy"  src="img/footer/footer__1.png" alt="footerphoto">
+                                            <img loading="lazy" src="img/footer/footer__1.png" alt="University News">
                                         </div>
                                         <div class="footerarea__right__content">
-                                            <span>02 Apr 2024 </span>
-                                            <h6>Best Your Business</h6>
+                                            <span>Latest News</span>
+                                            <h6>Academic Excellence Awards</h6>
                                         </div>
                                     </a>
                                 </li>
-
                                 <li>
                                     <a href="#">
                                         <div class="footerarea__right__img">
-                                            <img loading="lazy"  src="img/footer/footer__2.png" alt="footerphoto">
+                                            <img loading="lazy" src="img/footer/footer__2.png" alt="Research Updates">
                                         </div>
                                         <div class="footerarea__right__content">
-                                            <span>02 Apr 2024 </span>
-                                            <h6>Keep Your Business</h6>
+                                            <span>Research</span>
+                                            <h6>Innovation in Technology</h6>
                                         </div>
                                     </a>
                                 </li>
-
                                 <li>
                                     <a href="#">
                                         <div class="footerarea__right__img">
-                                            <img loading="lazy"  src="img/footer/footer__3.png" alt="footerphoto">
+                                            <img loading="lazy" src="img/footer/footer__3.png" alt="Student Success">
                                         </div>
                                         <div class="footerarea__right__content">
-                                            <span>02 Apr 2024 </span>
-                                            <h6>Nice Your Business</h6>
+                                            <span>Student Life</span>
+                                            <h6>Student Achievements</h6>
                                         </div>
                                     </a>
                                 </li>
@@ -127,16 +125,22 @@
                     </div>
                 </div>
             </div>
-        </div> --}}
+        </div>
+
+        <!-- Footer Bottom with Copyright and Developer Info -->
         <div class="footerarea__copyright__wrapper footerarea__copyright__wrapper__2">
             <div class="row">
-                <div class="col-xl-12 col-lg-12">
-                    <div class="text-center footerarea__copyright__content footerarea__copyright__content__2">
-                        <p>Copyright &copy; <span>{{ date('Y') }}</span> by CUG. All Rights Reserved.</p>
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                    <div class="footerarea__copyright__content footerarea__copyright__content__2">
+                        <p>&copy; <span>{{ date('Y') }}</span> Copperbelt University. All Rights Reserved.</p>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                    <div class="footerarea__developer__info text-end">
+                        <p>Developed and Powered by <a href="#" class="developer__link">Metascholar Consult LTD</a></p>
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
 </div>
