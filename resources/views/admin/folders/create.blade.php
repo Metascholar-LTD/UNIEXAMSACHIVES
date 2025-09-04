dd085cc4b1d646e19138857ebff18237a6f71a01
Kyiewu Bernard <kyiewubernard18@gmail.com>
2025-09-04 14:31:01 +0000

    chnges reverted

--- resources/views/admin/folders/create.blade.php
+++ resources/views/admin/folders/create.blade.php
@@ -10,7 +10,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
 <style>
-    .create-folder-hero, .create-folder-hero *, .form-section, .form-section * {
+    * {
         font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif;
     }
     
