c3ddfe4a7094707c7ae5364497c6b449526aeaed
Kyiewu Bernard <kyiewubernard18@gmail.com>
2025-10-07 18:08:23 +0000

    .

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -54,7 +54,7 @@
                                     <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
                                     <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
                                 </svg>
-                                Memos</a><span class="dashboard__label">{{$newMessagesCount}} / {{$totalMemosCount ?? 0}}</span>
+                               Received Memos</a><span class="dashboard__label">{{$newMessagesCount}} / {{$totalMemosCount ?? 0}}</span>
                         </li>
                     @endif
                 @endauth
