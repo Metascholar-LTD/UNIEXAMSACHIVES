ed5d1664364fcd3bbe877c3b02431f5dd26b812b
Kyiewu Bernard <kyiewubernard18@gmail.com>
2025-10-28 16:03:48 +0000

    export button made visible to all

--- resources/views/admin/uimms/chat.blade.php
+++ resources/views/admin/uimms/chat.blade.php
@@ -31,21 +31,19 @@
                                         </div>
                                     </a>
                                     
-                                    @if($memo->memo_status === 'completed')
-                                        <div class="header-separator"></div>
-                                        <button class="responsive-btn export-btn" onclick="exportChatConversation()">
-                                            <div class="svgWrapper">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="svgIcon">
-                                                    <path stroke="#fff" stroke-width="2" d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-                                                    <polyline stroke="#fff" stroke-width="2" points="14,2 14,8 20,8"></polyline>
-                                                    <line stroke="#fff" stroke-width="2" x1="16" y1="13" x2="8" y2="13"></line>
-                                                    <line stroke="#fff" stroke-width="2" x1="16" y1="17" x2="8" y2="17"></line>
-                                                    <polyline stroke="#fff" stroke-width="2" points="10,9 9,9 8,9"></polyline>
-                                                </svg>
-                                                <div class="text">Export and Print</div>
-                                            </div>
-                                        </button>
-                                    @endif
+                                    <div class="header-separator"></div>
+                                    <button class="responsive-btn export-btn" onclick="exportChatConversation()">
+                                        <div class="svgWrapper">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="svgIcon">
+                                                <path stroke="#fff" stroke-width="2" d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                                                <polyline stroke="#fff" stroke-width="2" points="14,2 14,8 20,8"></polyline>
+                                                <line stroke="#fff" stroke-width="2" x1="16" y1="13" x2="8" y2="13"></line>
+                                                <line stroke="#fff" stroke-width="2" x1="16" y1="17" x2="8" y2="17"></line>
+                                                <polyline stroke="#fff" stroke-width="2" points="10,9 9,9 8,9"></polyline>
+                                            </svg>
+                                            <div class="text">Export and Print</div>
+                                        </div>
+                                    </button>
                             </div>
                             <div class="chat-header-right">
                                 <div class="memo-status">
