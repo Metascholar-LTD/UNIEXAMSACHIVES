0fb7f153a2d8338a43f14de538ec5f6e4235de0b
Kyiewu Bernard <kyiewubernard18@gmail.com>
2025-11-06 15:06:33 +0000

    .

--- resources/views/super-admin/layout.blade.php
+++ resources/views/super-admin/layout.blade.php
@@ -6,6 +6,11 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Super Admin') - Metascholar Consult</title>
     
+    <!-- Favicon -->
+    <link rel="icon" type="image/jpeg" href="https://res.cloudinary.com/dsypclqxk/image/upload/v1756325702/Meta_fqxtsp.jpg">
+    <link rel="shortcut icon" type="image/jpeg" href="https://res.cloudinary.com/dsypclqxk/image/upload/v1756325702/Meta_fqxtsp.jpg">
+    <link rel="apple-touch-icon" href="https://res.cloudinary.com/dsypclqxk/image/upload/v1756325702/Meta_fqxtsp.jpg">
+    
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
