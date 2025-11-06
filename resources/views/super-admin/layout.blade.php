465e9fca9e324d35678fa719453e86a159f51afc
Kyiewu Bernard <kyiewubernard18@gmail.com>
2025-11-06 13:33:33 +0000

    .

--- resources/views/super-admin/layout.blade.php
+++ resources/views/super-admin/layout.blade.php
@@ -21,7 +21,7 @@
         }
 
         body {
-            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", "Noto Sans", "Liberation Sans", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
             background: #f8f9fa;
             color: #333;
         }
