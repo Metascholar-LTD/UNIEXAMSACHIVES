586ecffbfcc269093b46880b9f5d2c1e90bcbbf9
Kyiewu Bernard <kyiewubernard18@gmail.com>
2025-11-06 15:46:40 +0000

    .

--- resources/views/super-admin/maintenance/create.blade.php
+++ resources/views/super-admin/maintenance/create.blade.php
@@ -246,7 +246,12 @@
         <div class="modern-card">
             <div class="modern-card-header">
                 <h5>
-                    <i class="icofont-calendar"></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="display: inline-block; vertical-align: middle; margin-right: 0.5rem;">
+                        <rect width="18" height="18" x="3" y="4" rx="2" ry="2"></rect>
+                        <line x1="16" x2="16" y1="2" y2="6"></line>
+                        <line x1="8" x2="8" y1="2" y2="6"></line>
+                        <line x1="3" x2="21" y1="10" y2="10"></line>
+                    </svg>
                     Schedule
                 </h5>
             </div>
