a8d2ab7e74b80978497f5e0fa9f25142bb8c2797
Kyiewu Bernard <kyiewubernard18@gmail.com>
2025-11-11 14:48:09 +0000

    .

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -58,13 +58,7 @@
                 {{-- Keep in View - Bookmarked Memos --}}
                 <li>
                     <a class="{{ request()->routeIs('dashboard.uimms.keep-in-view') ? 'active' : '' }}" href="{{route('dashboard.uimms.keep-in-view')}}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                            viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                            class="feather feather-book-open">
-                            <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
-                            <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
-                        </svg>
+                        <img src="https://res.cloudinary.com/dsypclqxk/image/upload/v1762872342/image_pgg76v.png" alt="Keep in View" style="width: 24px; height: 24px; object-fit: contain;">
                         Keep in View
                     </a>
                 </li>
