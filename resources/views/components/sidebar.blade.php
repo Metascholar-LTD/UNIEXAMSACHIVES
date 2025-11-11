a12d390be89f0c81d620894b9d6b1b63a5c87065
Kyiewu Bernard <kyiewubernard18@gmail.com>
2025-11-11 14:44:35 +0000

    .

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -61,8 +61,9 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                             viewBox="0 0 24 24" fill="none" stroke="currentColor"
                             stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                            class="feather feather-bookmark">
-                            <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+                            class="feather feather-book-open">
+                            <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
+                            <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
                         </svg>
                         Keep in View
                     </a>
