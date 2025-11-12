388e46d29d345e97dd9c0f8aa0a4bf7e9bae8f6e
Kyiewu Bernard <kyiewubernard18@gmail.com>
2025-11-12 10:24:35 +0000

    .

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -387,13 +387,7 @@
                         </li>
                         <li>
                             <a class="{{ request()->routeIs('dashboard.payment-history.*') ? 'active' : '' }}" href="{{route('dashboard.payment-history.index')}}">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                    class="feather feather-credit-card">
-                                    <rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect>
-                                    <line x1="1" y1="10" x2="23" y2="10"></line>
-                                </svg>
+                                <img src="https://res.cloudinary.com/dsypclqxk/image/upload/v1762942944/fad007d4-0d21-4c41-9351-c06d179223f6.png" alt="Payment History" style="width: 18px; height: 18px; object-fit: contain; margin-right: 10px;">
                                 Payment History</a>
                         </li>
                     @endunless
