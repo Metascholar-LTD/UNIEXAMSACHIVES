4ab0b53912adb91eaa5e59d7c1a9eebaacb9c841
Kyiewu Bernard <kyiewubernard18@gmail.com>
2026-01-26 11:38:01 +0000

    .

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -89,15 +89,6 @@
    <script src="{{asset('js/main.js')}}"></script>
 
    <script>
-       // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-       if (localStorage.getItem("theme-color") === "dark" || (!("theme-color" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
-           document.getElementById("light--to-dark-button")?.classList.add("dark--mode");
-       }
-       if (localStorage.getItem("theme-color") === "light") {
-           document.getElementById("light--to-dark-button")?.classList.remove("dark--mode");
-       }
-   </script>
-   <script>
     tinymce.init({
       selector: 'textarea.tiny',
       plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
